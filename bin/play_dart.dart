2ef0fbe982680048681a3259ef32cc35e16c2075
Tanin Teerakitwattana <mypumz@gmail.com>
2024-01-05 14:21:52 +0700

    init

---
+++ bin/play_dart.dart
@@ -0,0 +1,5 @@
+import 'package:play_dart/play_dart.dart' as play_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${play_dart.calculate()}!');
+}
---
+++ test/play_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:play_dart/play_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
